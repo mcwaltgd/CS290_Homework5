db3eed8300857490938b352ec3769095a96bcd2c
mcwaltgd <55557803+mcwaltgd@users.noreply.github.com>
2021-05-13 07:48:34 -0400

    Add files via upload

--- HW5test2.js
+++ HW5test2.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 
-app.engine('handlebars', handlebars.engine);
+app.engine("handlebars", handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 31205);
 
